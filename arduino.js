458b80400de3a96e4a31cef4f2a93e11196cd8c2
Kenneth Cheng <kenthesage@gmail.com>
2013-04-07 10:41:06 -0400

    Add rumble on death

--- arduino.js
+++ arduino.js
@@ -3,7 +3,7 @@ var five = require('johnny-five');
 var client = require("socket.io-client", {
     reconnect: true
 });
-var socket = client.connect("http://pawns.jit.su");
+var socket = client.connect("http://localhost:8080");
 
 
 /* ARDUINO INPUT HANDLERS */
@@ -20,19 +20,13 @@ board.on('ready', function() {
     var greenButton = new five.Button(12);
     var whiteButton = new five.Button(13);
     var joystickButton = new five.Button(7);
-    var rumble = 9;
+    var rumble = new five.Motor(9);
 
     joystick.on('axismove', function( err, timestamp ) {
         socket.emit('tilt', {
             x: this.fixed.x,
             y: this.fixed.y
         });
-    });
-    joystickButton.on('up', function() {
-        self.firmata.digitalWrite(rumble, self.firmata.HIGH);
-    });
-    joystickButton.on('down', function() {
-        self.firmata.digitalWrite(rumble, self.firmata.LOW);
     });
 
     greenButton.on('up', function() {
@@ -42,4 +36,11 @@ board.on('ready', function() {
     whiteButton.on('up', function() {
         console.log("White button pressed!");
     });
+
+    socket.on('death', function() {
+        rumble.start();
+        board.wait(1000, function() {
+            rumble.stop();
+        })
+    })
 });
